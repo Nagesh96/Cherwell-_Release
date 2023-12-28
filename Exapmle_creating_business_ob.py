b9db11de01495cd83acc825d7f16739f85c13ac3
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-28 14:22:22 +0530

    Create Exapmle_creating_business_ob.py

---
+++ Exapmle_creating_business_ob.py
@@ -0,0 +1,45 @@
+{
+"busObId" : "6dd53665c0c24cab86870a21cf6434ae",
+"fields" : [{
+"fieldId" : "93670bdf8abe2cd1f92b1f490a90c7b7d684222e13",
+"name" : "Call Source",
+"value" : "Event",
+"dirty" : true
+}, {
+"fieldId" : "936725cd10c735d1dd8c5b4cd4969cb0bd833655f4",
+"name" : "Service",
+"value" : "Printing",
+"dirty" : true
+}, {
+"fieldId" : "9e0b434034e94781ab29598150f388aa",
+"name" : "Category",
+"value" : "Desktop",
+"dirty" : true
+}, {
+"fieldId" : "1163fda7e6a44f40bb94d2b47cc58f46",
+"name" : "Subcategory",
+"value" : "Submit Incident",
+"dirty" : true
+}, {
+"fieldId" : "93db6da57bde4b909d98d340d59e22c974abd9c903",
+"name" : "CustomerDisplayName",
+"value" : "Default Customer",
+"dirty" : true
+}, {
+"fieldId" : "252b836fc72c4149915053ca1131d138",
+"name" : "Description",
+"html" : "Line 1. </br> Line 2.",
+"dirty" : true
+}, {
+"fieldId" : "93e8ea93ff67fd95118255419690a50ef2d56f910c",
+"name" : "ShortDescription",
+"value" : "Test 3",
+"dirty" : true
+}, {
+"fieldId" : "83c36313e97b4e6b9028aff3b401b71c",
+"name" : "Priority",
+"value" : "3",
+"dirty" : true
+}
+]
+} 
