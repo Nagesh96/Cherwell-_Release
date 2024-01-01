4cc1b13a86c4d6be335d5ff08eab04db21cb469b
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-01-01 07:32:57 +0530

    Create harness_busobid.py

---
+++ harness_busobid.py
@@ -0,0 +1,20 @@
+# Cherwell API details
+baseUrl="https://charter.cherwellondemand.com"
+CLIENT_ID="<+secrets.getValue("account.ARAClientPROD_Client_ID")>"
+USERNAME="ARAClientPROD"
+PASSWORD="<+secrets.getValue("account.ARAClientPROD")>"
+
+# Get Authorization token
+AUTH_TOKEN_RESPONSE=$(curl -s -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" -d "grant_type=password&client_id=$CLIENT_ID&username=$USERNAME&password=$PASSWORD" "$baseUrl/CherwellAPI/token"| jq -r '.access_token')
+
+echo "Current API Token:" $AUTH_TOKEN_RESPONSE
+
+# Continue with the rest of the script...
+
+# Business Object details
+BUSINESS_OBJECT_NAME="ChangeRequest"
+
+# Fetch busObId for the Change Request Business Object
+BUSOBID=$(curl -s -X GET "$CHERWELL_URL/busobdefinition?busobname=$BUSINESS_OBJECT_NAME" -H "Authorization: $AUTH_TOKEN_RESPONSE" | jq -r '.busObId')
+
+echo "busObId for $BUSINESS_OBJECT_NAME: $BUSOBID"
